fb2b67bc5e058570a471d78cd24fae8e1f346e07
Kavia AI <production@kavia.ai>
2025-06-07 16:04:52 +0000

    CheckPoint

--- weekendwayfinder_frontend/src/app/pages/home/home.component.ts
+++ weekendwayfinder_frontend/src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 // PUBLIC_INTERFACE
 @Component({
@@ -9,4 +10,15 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {}
+export class HomeComponent {
+  // eslint-disable-next-line no-unused-vars
+  constructor(public router: Router) {}
+
+  // PUBLIC_INTERFACE
+  /**
+   * Navigates to the Trip Planner page ('/trip-planner') when called.
+   */
+  goToTripPlanner(): void {
+    this.router.navigate(['/trip-planner']);
+  }
+}
